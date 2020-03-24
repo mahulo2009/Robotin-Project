04c177e0bf8145561ff6e4ecce36b646c43a5d6a
mhuertas <manuel.huertas.lopez@gmail.com>
2020-03-24 19:38:28 +0000

    Include in the launch file the volume to Robotin Project

--- docker_images/ubuntu/bionic/robot-perception/launch_container.sh
+++ docker_images/ubuntu/bionic/robot-perception/launch_container.sh
@@ -1,5 +1,6 @@
 docker run -it --rm    \
 	--network=host \
+	-v /home/mhuertas/Work/Robotin-Project/robotin_project:/root/catkin_ws/src/robotin_project \
 	ubuntu/ros:melodic-robot-perception \
         bash	
 
